8ce6b53d36bedbcc4f57d62ed329a38bbb11a5dd
HassanIyan <iyan@javaabu.com>
2023-10-18 14:06:13 +0500

    CSRF middleware added

--- src/middlewares/sikundi.ts
+++ src/middlewares/sikundi.ts
@@ -1,7 +1,10 @@
-import type { NextFetchEvent, NextMiddleware, NextRequest } from 'next/server'
+import { NextResponse, type NextFetchEvent, type NextMiddleware, type NextRequest } from 'next/server'
+import { headers } from 'next/headers'
 
 export function sikundiMiddleware(middleware: NextMiddleware) {
     return async (request: NextRequest, event: NextFetchEvent) => {
+        const headersList = headers()
+        const csrf = headersList.get('X-CSRF-Token')
         if(request.nextUrl.pathname.startsWith("/sikundi-login") || request.nextUrl.pathname.startsWith("/sikundi-admin")) {
 
 
@@ -9,6 +12,20 @@ export function sikundiMiddleware(middleware: NextMiddleware) {
 
 
             
+            
+        }
+        if(String(request.method).toUpperCase() !== "GET" && csrf !== process.env.CSRF_SECRET) {
+            return NextResponse.json({ 
+                error: {
+                    name: "Authentication Error",
+                    detail: "CSRF Missing"
+                },
+                notification: {
+                    title: 'Auth error',
+                    description: 'your csrf token is missing. please reload and try again'
+                }
+            }, { status: 401 })
+
         }
         return middleware(request, event)
     }
--- src/middlewares/web.ts
+++ src/middlewares/web.ts
@@ -1,7 +1,10 @@
-import type { NextFetchEvent, NextMiddleware, NextRequest } from 'next/server'
+import { NextResponse, type NextFetchEvent, type NextMiddleware, type NextRequest } from 'next/server'
+import { headers } from 'next/headers'
 
 export function webMiddleware(middleware: NextMiddleware) {
     return async (request: NextRequest, event: NextFetchEvent) => {
+        const headersList = headers()
+        const csrf = headersList.get('X-CSRF-Token')
         if(!request.nextUrl.pathname.startsWith("/sikundi-login") && !request.nextUrl.pathname.startsWith("/sikundi-admin")) {
             
 
@@ -10,6 +13,19 @@ export function webMiddleware(middleware: NextMiddleware) {
 
 
             
+        }
+        if(String(request.method).toUpperCase() !== "GET" && csrf !== process.env.CSRF_SECRET) {
+            return NextResponse.json({ 
+                error: {
+                    name: "Authentication Error",
+                    detail: "CSRF Missing"
+                },
+                notification: {
+                    title: 'Auth error',
+                    description: 'your csrf token is missing. please reload and try again'
+                }
+            }, { status: 401 })
+
         }
         return middleware(request, event)
     }
