a74f8623ac1aa4475b39c7fbe45ff5afb2c3f92e
HassanIyan <iyan@javaabu.com>
2023-10-23 17:50:50 +0500

    added

--- src/app/(sikundi)/layout.tsx
+++ src/app/(sikundi)/layout.tsx
@@ -122,7 +122,7 @@ export default function RootLayout(props: Props) {
                     showSpinner={true}
                     easing="ease"
                     speed={200}
-                    shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+                    shadow="0 0 10px #000000,0 0 5px #000000"
                     template='<div class="bar" role="bar"><div class="peg"></div></div> 
                     <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
                     zIndex={1600}
