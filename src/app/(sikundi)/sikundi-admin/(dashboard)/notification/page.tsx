6202fb53295cfc65fd8207681cebba4cc343564f
HassanIyan <iyan@javaabu.com>
2023-10-25 14:52:56 +0500

    select2 component

--- src/app/(sikundi)/sikundi-admin/(dashboard)/notification/page.tsx
+++ src/app/(sikundi)/sikundi-admin/(dashboard)/notification/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { SelectComponent } from "@sikundi/components/ui/ReactSelect"
 import { Alert, AlertDescription, AlertTitle } from "@sikundi/components/ui/alert"
 import { Brain } from "lucide-react"
 import { Fragment } from "react"
@@ -12,6 +15,19 @@ export default async function Notifications() {
                     In there sidebar you can access all the collections and plugins you have access to.
                 </AlertDescription>
             </Alert>
+            <SelectComponent
+                createAble={true}
+                isMulti={true}
+                asyncLoading
+                defaultOptions={[
+                    {value: "1", label: "1"},
+                    {value: "2", label: "2"},
+                    {value: "3", label: "3"},
+                    {value: "4", label: "4"},
+                    {value: "5", label: "5"},
+                ]}
+                placeholder="Select Skills"
+            />
         </Fragment>
     )
 }
