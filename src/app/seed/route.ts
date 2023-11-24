e57790332d904aad263ebad386ffea372dc65685
iyaaan <hassan.iyan.l@gmail.com>
2023-11-24 20:32:32 +0500

    aded

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse, type NextRequest } from 'next/server'
 import seed from '@sikundi/seeders'
 import { prisma } from '@sikundi/lib/server/utils/prisma'
+import filestream, { promises as fs } from "fs"
+import axios from 'axios'
 
 export async function GET(request: NextRequest) {
     if (process.env.NODE_ENV === "development") {
@@ -51,8 +53,40 @@ export async function POST(request: NextRequest) {
         }
     })
 
+    if(data?.featureImage?.connectOrCreate?.create?.url) {
+        const directoryPath = data.featureImage.connectOrCreate.create.url?.split("/")?.slice(0, 5)?.join("/")?.replace("/sikundi-content/", "./storage/")
+        const url = data.featureImage.connectOrCreate.create.url?.replace("/sikundi-content/", "https://gaafu.mv/wp-content/")
+        try {
+            await fs.access(directoryPath)
+        } catch (error) {
+            await fs.mkdir(directoryPath, { recursive: true })
+        } finally {
+            await downloadImage(url, directoryPath)
+        }
+    }
+
     console.log(post)
     return NextResponse.json({seed: true}, {
         status: 200
     })
 }
+
+
+async function downloadImage(url:string, directoryPath:string) {
+    try {
+        const response = await axios({
+            method: 'GET',
+            url,
+            responseType: 'stream'  
+        });
+        const writer = filestream.createWriteStream(`${directoryPath}/${url.split('/')[url.split('/').length - 1]}`);
+        response.data.pipe(writer);
+        return new Promise((resolve, reject) => {
+            writer.on('finish', resolve);
+            writer.on('error', reject);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  
+  }
