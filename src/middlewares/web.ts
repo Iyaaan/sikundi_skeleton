6e6820f18a1215a3d11f7bec0256d60b6e688d2d
HassanIyan <iyan@javaabu.com>
2023-11-25 14:31:28 +0500

    added

--- src/middlewares/web.ts
+++ src/middlewares/web.ts
@@ -3,16 +3,16 @@ import { NextResponse, type NextFetchEvent, type NextMiddleware, type NextReques
 export function webMiddleware(middleware: NextMiddleware) {
     return async (request: NextRequest, event: NextFetchEvent) => {
         if(!request.nextUrl.pathname.startsWith("/sikundi-login") && !request.nextUrl.pathname.startsWith("/sikundi-admin")) {
-            // if (
-            //     request.nextUrl.pathname === "/" 
-            // ) {
-            //     return NextResponse.redirect(new URL('/dv', request.url))
-            // }
+            if (
+                request.nextUrl.pathname === "/" 
+            ) {
+                return NextResponse.redirect(new URL('/dv', request.url))
+            }
 
-            // const regex = /^\/\d+$/;
-            // if(regex.test(request.nextUrl.pathname)) {
-            //     return NextResponse.redirect(new URL(`/dv${request.nextUrl.pathname.match(regex)}`, request.url))
-            // }
+            const regex = /^\/\d+$/;
+            if(regex.test(request.nextUrl.pathname)) {
+                return NextResponse.redirect(new URL(`/dv${request.nextUrl.pathname.match(regex)}`, request.url))
+            }
         }
         return middleware(request, event)
     }
