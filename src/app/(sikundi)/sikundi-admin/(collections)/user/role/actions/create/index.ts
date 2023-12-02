e1260cc6a4787b3db818b5b33411b683ca720e4e
HassanIyan <iyan@javaabu.com>
2023-12-02 11:22:46 +0500

    added

--- src/app/(sikundi)/sikundi-admin/(collections)/user/role/actions/create/index.ts
+++ src/app/(sikundi)/sikundi-admin/(collections)/user/role/actions/create/index.ts
@@ -1,13 +1,13 @@
 "use server"
 
 import getUser from '@sikundi/lib/server/utils/getUser'
-import UserSchema, { UserSchemaType } from './schema'
+import RoleSchema, { RoleSchemaType } from './schema'
 import ErrorHandler from '@sikundi/lib/server/utils/ErrorHandler'
 import { prisma } from '@sikundi/lib/server/utils/prisma'
 import { revalidatePath } from 'next/cache'
 
-export default async function POST(data: UserSchemaType) {
-    return (await ErrorHandler(data, UserSchema, async (data:UserSchemaType) => {
+export default async function POST(data: RoleSchemaType) {
+    return (await ErrorHandler(data, RoleSchema, async (data:RoleSchemaType) => {
         const usr = await getUser()
         
         throw({})
