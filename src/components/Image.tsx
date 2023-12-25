01f9cf4e3376b6b473263c435a47881ed921a817
HassanIyan <iyan@javaabu.com>
2023-12-25 17:38:35 +0500

    added

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -10,6 +10,7 @@ export default function Image({cdn = false, ...props}:Props) {
         <NextImage
             {...props}
             src={`${(process.env.NEXT_PUBLIC_CDN_URL && cdn) ? process.env.NEXT_PUBLIC_CDN_URL : ''}${props.src}`}
+            sizes='50vw'
         />
     )
 }
