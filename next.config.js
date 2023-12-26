dd112a4b464bbb65ed33469e27df208c2eb94228
iyaaan <hassan.iyan.l@gmail.com>
2023-12-26 21:49:19 +0500

    added

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/*',
+                source: '/:path*',
                 headers: [
                     {
                         key: 'access-control-allow-origin',
