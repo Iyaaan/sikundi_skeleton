43a309476209d365e083a2fd9157308154f83796
iyaaan <hassan.iyan.l@gmail.com>
2023-12-26 21:22:51 +0500

    added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
     experimental: {
         serverActions: {
@@ -18,16 +21,7 @@ const nextConfig = {
             ]
         }
     },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cdn.gaafu.media',
-                // port: '',
-                pathname: '/**', 
-            }
-        ]
-    }
+    assetPrefix: isProd ? 'https://cdn.gaafu.media' : undefined,
 }
 
 module.exports = nextConfig
