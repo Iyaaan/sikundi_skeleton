bcc902f4fe5a2692bc63c8f4ced0c2d15b5734e1
HassanIyan <iyan@javaabu.com>
2023-12-31 11:35:01 +0500

    layout created for vareint three

--- src/app/(web)/en/_components/cards/PostCard.tsx
+++ src/app/(web)/en/_components/cards/PostCard.tsx
@@ -1,5 +1,6 @@
 import React, { AnchorHTMLAttributes, FC } from 'react'
 import Link, { LinkProps } from 'next/link'
+import Image from 'next/image'
 
 type Props = AnchorHTMLAttributes<HTMLAnchorElement> & LinkProps & {
     data: {
@@ -12,10 +13,18 @@ type Props = AnchorHTMLAttributes<HTMLAnchorElement> & LinkProps & {
     }
 }
 
-const PostCard:FC<Props> = ({ ...props }) => {
+const PostCard:FC<Props> = ({ data, ...props }) => {
     return (
         <Link {...props}>
-            
+            <span className='block relative'>
+                {data.featureImageUrl && <Image src={data.featureImageUrl} fill sizes='75vw' alt={data.title} /> }
+            </span>
+            <span>{data.category}</span>
+            <b>{data.title}</b>
+            <span>
+                <span></span>
+                {data?.createdBy && `by ${data.createdBy.name}`}
+            </span>
         </Link>
     )
 }
